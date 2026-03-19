4cd22f39f1c45353341ef3c9ddcbe1abb4c49e68
MLN-1994 <marianolangge@gmail.com>
2026-03-19 14:18:08 -0300

    feat(layout): Add Facebook Pixel tracking script to enhance analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,32 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '915119514646115');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* @ts-ignore */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=915119514646115&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
       </head>
       <body className={`${lexend.variable} font-sans antialiased`}>
         <div className="min-h-screen flex flex-col">
